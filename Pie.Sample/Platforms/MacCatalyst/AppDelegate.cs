5b7cec74d87d8538749cf7fa43eba13e4d4f373a
Pablo <pablo.erick@xpand-it.com>
2023-10-19 15:00:07 +0100

    first commit

---
+++ Pie.Sample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Pie.Sample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
